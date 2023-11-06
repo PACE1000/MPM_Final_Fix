4a845d6b10b2be843b471f5da6732638cd42f664
Arya Bramaputra <aryabrama901@gmail.com>
2023-11-06 21:16:43 +0700

    first commit

---
+++ challenge/lib/Screen/LoginScreen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget{
+  LoginScreen({Key? key}):super(key: key);
+
+  @override
+  _LoginScreen createState() => _LoginScreen();
+}
+
+class _LoginScreen extends State<LoginScreen>{
+  final TextEditingController username = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: 
+      Column(
+        children: <Widget>[
+          //Login Text
+          Container(
+            padding: EdgeInsets.only(top: 20,left: 20,right: 20),
+            alignment: Alignment.center,
+            child: Text("LOGIN",style: TextStyle(color: Colors.white,fontSize: 30, fontFamily: 'poppins_r'),),
+          ),
+
+          //Username
+          Container(
+            padding: EdgeInsets.only(left: 20,right: 20,top: 10),
+            alignment: Alignment.center,
+            child: TextField(
+              controller: username,
+              decoration: InputDecoration(
+                hintText: "Masukkan Nama", 
+                hintStyle: TextStyle(
+                  fontFamily: 'poppins_r'),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.purple),),
+                    enabledBorder:OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black),),
+                        filled: true,
+                        fillColor: Colors.white
+                          ),
+            ),
+          ),
+
+          //Password
+          Container(
+            padding: EdgeInsets.only(left: 20,right: 20,top: 10),
+            child: TextField(
+              decoration: InputDecoration(hintText: "Masukkan Password",hintStyle: TextStyle(fontFamily: 'poppins_r'),
+              focusedBorder:OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+              enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+              fillColor: Colors.white,
+              filled: true ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+} 
